dfe43feae39903706c31defa8d5807ea0b9c46c4
Romy Huzen <47813347+romyhuzen@users.noreply.github.com>
2019-09-11 10:53:56 +0200

    homepage

---
+++ view/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            
+            <link rel="stylesheet" href="assets/style.css" type="text/css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+            <title>Gameplay Party</title>
+        </head>
+
+        <header>
+            <img class="logo" src="view/images/gpp.svg" alt="Gameplay Party">
+            <h2 class="title">Gameplay Party</h2><br>
+        </header>
+
+    </html>
---
+++ view/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html>
+        <main>
+            <div class="content">
+                <h1 class="con_title">Wat is Gameplay Party?</h1>
+                <p class="con_in">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <button class="accents"><a href='view/.php'>Lees meer</a></button>
+            </div>
+
+            <div class="content">
+                <h1 class="con_title">hello</h1>
+                <p class="con_in">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <button class="accents"><a href='view/.php'>Lees meer</a></button>
+            </div>
+        </main>
+    </html>
