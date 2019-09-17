c9cf2cc95315688f996ba24ab82b0ab328d244c0
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-17 16:23:23 +0200

    addd a header for beheerder

---
+++ view/beheerder/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+  <title>Gameplay Party</title>
+</head>
+
+<header>
+  <img class="logo" src="view/images/gpp.svg" alt="Gameplay Party">
+  <h2 class="title">Beheerder</h2><br>
+  <!-- <nav>
+    <a href="" onclick=""><button>Bioscopen overzicht</button></a>
+    <a href="index.php?op=loginForm"><button>Logout</button></a></nav> -->
+</header>
+
+
+
+</html>
