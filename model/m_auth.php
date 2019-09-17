a4f78750d8d1e576a7a74c2771ae4d32a69316d6
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-17 16:26:30 +0200

    made a login for beheerder with session

--- model/m_auth.php
+++ model/m_auth.php
@@ -2,17 +2,29 @@
 
 class AuthModel
 {
-  public function getloginRedact()
+  public function getUser()
   {
-    // here goes some hardcoded values to simulate the database
-    if (isset($_REQUEST['Rusername']) && isset($_REQUEST['Rpassword'])) {
-      if ($_REQUEST['Rusername'] == 'admin' && $_REQUEST['Rpassword'] == 'admin') {
-        require_once ("controller/c_gpp.php");
-        $GPPController = new GPPController();
-        return $GPPController->handleRequest();
-      } else {
-        return 'invalid user';
+    if (!empty($_POST)) {
+      if (isset($_POST['username']) && isset($_POST['password'])) {
+        $hashed_password = hash('ripemd160', $_POST['password']);
+        // Getting submitted user data from database
+        $con = new mysqli("localhost", "root", "", "gpp");
+        $stmt = $con->prepare("SELECT * FROM users WHERE username = ? ");
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_object();
+
+        // Verify user password and set $_SESSION
+        if ($hashed_password == $user->password) {
+          $_SESSION['user_id'] = $user->user_id;
+          $_SESSION['user_role'] = $user->role;
+          return 'login';
+        } else {
+          echo "password False";
+        }
       }
-    }
+    
   }
+}
 }
