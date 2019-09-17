fa7f2412120dbeb5c6b6e9abefee768ed3efbd71
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-17 16:25:16 +0200

    set 2 cases for handelrequest

--- controller/c_gpp.php
+++ controller/c_gpp.php
@@ -1,28 +1,41 @@
 <?php
 require_once 'model/m_GPPLogic.php';
 
-class GPPController {
-	
-	public function __construct(){
+class GPPController
+{
+
+	public function __construct()
+	{
 		$this->GPPLogic = new GPPLogic();
 	}
 
-	public function __destruct(){}
+	public function __destruct()
+	{ }
 
-	public function handleRequest(){
+	public function handleRequest()
+	{
 		try {
 			$op = isset($_REQUEST['op']) ? $_REQUEST['op'] : NULL;
 			switch ($op) {
-				default:
+				case 'beOverzicht':
+					$this->collectBioscopen();
+					break;
+				case 'beHome':
 					$this->collectHome();
 					break;
 			}
 		} catch (ValidationException $e) {
-			$errors = $e->getErrors();
+			// $errors = $e->getErrors();
 		}
 	}
 
-	public function collectHome() {
+	public function collectHome()
+	{
 		include 'view/beheerder/beheerder.php';
+	}
+
+	public function collectBioscopen()
+	{
+		include 'view/overzichtBios.php';
 	}
 }
