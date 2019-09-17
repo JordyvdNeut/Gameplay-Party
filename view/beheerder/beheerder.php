94d5eb005e086f45d52997a0e47df53e3f1b8326
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-17 16:25:35 +0200

    add some content for beheerder

--- view/beheerder/beheerder.php
+++ view/beheerder/beheerder.php
@@ -1,6 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<h2>Beheerder</h2>
-  <section></section>
+<?php
+$_SESSION['user_role'];
+if (isset($_SESSION['user_role']) == 3) {
+  ?>
+  <?php require_once "view/beheerder/header.php"; ?>
+
+  <section>
+    <a style="color: #2c3e50;" href="#">
+      <div class="login">
+        <h2>Content overzicht</h2>
+        Hier vind u een overzicht van alle content in de website. Deze content kunt u bewerken.
+      </div>
+    </a>
+  </section>
+
+<?php
+} else {
+  // Redirect them to the login page
+  header("Location: index.php?op=loginForm");
+}
+?>
+
 </html>
