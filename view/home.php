94c0a47ed322c12b0feaba85eb5b17eaab6f2c33
JurredeBruin <50706153+JurredeBruin@users.noreply.github.com>
2019-09-20 10:46:19 +0200

    grid  toevoeging margin

--- view/home.php
+++ view/home.php
@@ -4,7 +4,8 @@
 <main>
 <div class="center">
 <div class="row">
-    <div class="content col-12">
+    <div class=" col-12">
+        <div class="content">
         <h1 class="con_title">Wat is Gameplay Party?</h1>
         <p class="con_in">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -14,8 +15,10 @@
         </p>
         <button class="accents"><a href='?op=overons'>Lees meer</a></button>
     </div>
+</div>
 
-    <div class="content col-12">
+    <div class=" col-12">
+        <div class="content">
         <h1 class="con_title">Bekijk hier het bioscoopoverzicht</h1>
         <p class="con_in">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -25,6 +28,7 @@
         </p>
         <button class="accents"><a href='?op=overzicht'>Lees meer</a></button>
     </div>
+</div>
 </div>
 </div>
 </main>
--- view/over_ons.php
+++ view/over_ons.php
@@ -9,10 +9,11 @@
     <link rel="stylesheet" href="../style.css" type="text/css">
 </head>
         <main>
-            <div class="content overons">
-
+        <div class="center">
+        <div class="row">
+            <div class="col-12">
+                <div class="content overons">
                 <h1 class="con_title">Over ons</h1>
-
                 <p class="con_in">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -21,13 +22,18 @@
                 </p>
                 <button class="accents"><a href='view/.php'>Lees meer</a></button>
             </div>
+            </div>
 
-            <div class="content email">
+            <div class="col-12">
+                <div class="content email">
                 <h1 class="con_title">Email</h1>
                 <p class="con_in">
-             zoek contact op via onze email <a href='mailto:testing@testing.com'>hier</a>
+             zoek contact op via onze email 
                 </p>
                 <button class="accents"><a href='view/.php'>Lees meer</a></button>
+            </div>
+            </div>
+            </div>
             </div>
         </main>
     </html>
