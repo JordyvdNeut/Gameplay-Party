c46e0e4264a25941ff3ead4e8fb8085e4d7dbbb1
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-23 09:19:24 +0200

    volgorde veranderd en login er tijdelijk uitgehaald

--- view/header.php
+++ view/header.php
@@ -16,17 +16,17 @@
   <nav>
     <div class="not-mobile">
       <a href="index.php?op=home"><button class="btn">Home</button></a>
-      <a href="index.php?op=overons"><button class="btn">Over ons</button></a>
       <a href="index.php?op=overzicht"><button class="btn">Bioscopen</button></a>
-      <a href="index.php?op=loginForm"><button class="btn">Login</button></a>
+      <a href="index.php?op=overons"><button class="btn">Over ons</button></a>
+      <!-- <button href="index.php?op=loginForm"><button class="btn">Login</button></a> -->
     </div>
     <div class="dropdown, mobile">
       <i class="fa fa-bars dropbtn" onclick="burgerMenu()"></i>
       <div id="myDropdown" class="dropdown-content">
         <a href="index.php?op=home">Home</a>
-        <a href="index.php?op=overons">Over ons</a>
         <a href="index.php?op=overzicht">Bioscopen</a>
-        <a href="index.php?op=loginForm">Login</a>
+        <a href="index.php?op=overons">Over ons</a>
+        <!-- <a href="index.php?op=loginForm">Login</a> -->
       </div>
     </div>
   </nav>
