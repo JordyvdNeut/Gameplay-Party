8771d9272dca5f9d804541a4757963fe8179bdaa
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-23 10:24:14 +0200

    Classes met hoofdletters, wat we niet gebruiken er uit gecomment

--- controller/c_bioscopen.php
+++ controller/c_bioscopen.php
@@ -2,98 +2,90 @@
 
 require_once('model/m_bioscopen.php');
 
-class biosController{
-    public function __construct() {
-		$this->bioscopen = new bioscopen();
+class BiosController
+{
+	public function __construct()
+	{
+		$this->bioscopen = new Bioscopen();
 	}
 
-    public function __destruct() {}
-        
-	public function handleRequest() {
+	public function __destruct()
+	{ }
+
+	public function handleRequest()
+	{
 		try {
 			$op = isset($_REQUEST['op']) ? $_REQUEST['op'] : NULL;
 			switch ($op) {
-				case 'update':
-					$this->update();
-					break;
-				case 'readBios':
-					$this->readBios();
-					break;
-				case 'delBios':
-					$this->deleteBios();
-					break;
-				case 'addBios':
-					$this->addBios();
-					break;
+					// case 'addBios':
+					// 	$this->addBios();
+					// 	break;
 				case 'overzicht':
 					$this->collectOverzicht();
 					break;
-				default:
-					$this->collectHome();
+				case 'detail':
+					$this->readBios($_REQUEST['id']);
 					break;
+					// case 'update':
+					// 	$this->update();
+					// 	break;
+					// case 'delBios':
+					// 	$this->deleteBios();
+					// 	break;
 			}
 		} catch (ValidationException $e) {
 			$errors = $e->getErrors();
 		}
-    }
-
-    public function collectHome() {
-		include 'view/beheerder/beheerder.php';
-	}
-    
-    public function update() {
-		$result = $this->bioscopen->update();
-		include 'view/beheerder/beheerder.php';
 	}
 
-	public function readBios() {
-		$result = $this->bioscopen->readsBioscopen();
-		include 'view/overzicht.php';
+	// public function addBios()
+	// {
+	// 	$result = $this->bioscopen->addBios();
+	// 	include 'view/beheerder/addbios.php';
+	// }
+
+	public function collectOverzicht()
+	{
+		$bioscopen = $this->bioscopen->readBioscopen();
+		$biosTable = $this->createDiv($bioscopen);
+		include_once 'view/overzicht.php';
 	}
 
-	public function collectOverzicht(){
-		$products = $this->bioscopen->readProducts();
-		include 'view/overzicht.php';
-   }
+	// public function readBios() {
+	// 	$result = $this->bioscopen->readBioscoop();
+	// 	include 'view/overzicht.php';
+	// }
+
+	// public function update()
+	// {
+	// 	$result = $this->bioscopen->update();
+	// 	include 'view/beheerder/beheerder.php';
+	// }
 
 
-   public function createTable($result){
-	$tableheader = false;
-	$html = "";
-	$html .= "<table>";
+	public function createDiv($result)
+	{
+		$html = "";
+		$html .= "<div class='center'><div class='row'>";
 
-	while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-		
-		if($tableheader == false) {
-			$html .= "<tr>";
-			
-			foreach($row as $key=>$value) {
-				$html .= "<th>{$key}</th>";
-			}
-			
-			$html .= "</tr>";
-			$tableheader = true;
+		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			$html .= "<div class='col-6'>";
+			$html .= "<div class='content'>";
+			$html .= "<h1 class='con_title'>$row[bios_naam]</h1>";
+			$html .= "<h2>$row[bios_plaats]</h2>";
+			$html .= "<p class='con_in'><img class='biosPhoto' src='$row[bios_foto]'></p>";
+			$html .= "<a href='index.php?op=detail&id=$row[bios_id]'><button class='accents, btn'>Lees meer</button></a>";
+			$html .= "</div>";
+			$html .= "</div>";
 		}
-		
-		foreach($row as $value) {
-			$html .= "<td>{$value}</td>";
-		}
-	
-		$html .= "</tr>";
+
+		$html .= "</div></div>";
+		return $html;
 	}
-	
-	$html .= "</table>";
-	return $html;
-}
 
-	public function addBios() {
-		$result = $this->bioscopen->addBios();
-		include 'view/beheerder/addbios.php';
-	} 
-
-	public function deleteBios() {
+	public function deleteBios()
+	{
 		$result = $this->bioscopen->delete();
 		include 'view/beheerder/deleteBios.php';
 	}
-
 }
