276d819f871c1dcdefc19f965625b09975997fa1
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-23 10:26:44 +0200

    classes met hoofdletter

--- model/m_bioscopen.php
+++ model/m_bioscopen.php
@@ -2,62 +2,67 @@
 
 require_once('model/m_DataHandler.php');
 
-class bioscopen{
+class Bioscopen
+{
 
-    public function __construct() {
-        $this->DataHandler = new DataHandler("localhost", "mysql", "gpp", "root", "");
+  public function __construct()
+  {
+    $this->DataHandler = new DataHandler("localhost", "mysql", "gpp", "root", "");
+  }
+
+  public function __destruct()
+  { }
+
+  // public function addBios(){
+  //     try {
+  //       /*$sql = 'INSERT INTO * FROM ';
+  //       $result = $this->DataHandler->createData($sql);
+  //       return $result;*/
+  //      } catch (exception $e) {
+  //       throw $e;
+  //     }
+  //   }
+
+  public function readBioscopen()
+  {
+    try {
+      $sql = "SELECT * FROM bioscopen";
+      $results = $this->DataHandler->readsData($sql);
+      return $results;
+    } catch (exception $e) {
+      throw $e;
     }
+  }
 
-    public function __destruct(){}
-
-    public function addBios(){
-        try {
-          /*$sql = 'INSERT INTO * FROM ';
-          $result = $this->DataHandler->createData($sql);
-          return $result;*/
-         } catch (exception $e) {
-          throw $e;
-        }
-      }
-    
-      public function readProducts() {
-        try{
-            $sql = 'SELECT * FROM bioscopen';
-            $results = $this->DataHandler->readsData($sql);
-            return $results;
-        } catch (exception $e){throw $e;} 
-    }
-    
-      public function readBois($id){
-        try {
-            /*$sql = 'SELECT * FROM bioscopen WHERE id =  ';
+  public function readBois($id)
+  {
+    try {
+      $sql = "SELECT * FROM bioscopen WHERE id = $id";
           $result = $this->DataHandler->readData($sql);
-          return $result;*/
+          return $result;
         } catch (exception $e) {
-          throw $e;
-        }
-      }
-    
-      public function update($id){
-        try {
-          /*$sql = 'UPDATE * SET WHERE id =  ';
-          $result = $this->DataHandler->updateData($sql);
-          return $result;*/
-         } catch (exception $e) {
-          throw $e;
-        }
-      }
-    
-      public function delete($id){
-        try {
-          /*$sql = 'DELETE * FROM bioscopen WHERE id = ';
-          $result = $this->DataHandler->deleteData($sql);
-          return $result;*/
-    
-         } catch (exception $e) {
-          throw $e;
-        }
-      }
+      throw $e;
+    }
+  }
+
+  // public function update($id){
+  //   try {
+  //     /*$sql = 'UPDATE * SET WHERE id =  ';
+  //     $result = $this->DataHandler->updateData($sql);
+  //     return $result;*/
+  //    } catch (exception $e) {
+  //     throw $e;
+  //   }
+  // }
+
+  // public function delete($id){
+  //   try {
+  //     /*$sql = 'DELETE * FROM bioscopen WHERE id = ';
+  //     $result = $this->DataHandler->deleteData($sql);
+  //     return $result;*/
+
+  //    } catch (exception $e) {
+  //     throw $e;
+  //   }
+  // }
 }
-
-
