49a8d471bfe729eb776b0c7c1adac319078a66eb
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-23 14:47:01 +0200

    format

--- view/footer.php
+++ view/footer.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
-    <html>
-       <footer>
-         <section>
-         <div class="center_footer">
-         <div class="row">
-           <div class="contact_foot col-6">
-             <h3><a class='footer' href="?op=overons">Contact</a></h3>
-             <ul class='footer'>
-             <li>Wij zijn bereikbaar op het email adres: </li>
-            </ul>
-           </div>
+<html>
+<footer>
+  <section>
+    <div class="center_footer">
+      <div class="row">
+        <div class="contact_foot col-6">
+          <h3><a class='footer' href="?op=overons">Contact</a></h3>
+          <ul class='footer'>
+            <li>Wij zijn bereikbaar op het email adres: </li>
+          </ul>
+        </div>
 
-            <div class="voorw_foot col-6">
-            <h3>Voorwaarden</h3>
-              <ul class='footer' >
-             <li><a class="footer" href="">Privacy Voorwaarden</a></li>
-             <li><a class="footer" href="">Privacy Beleid</a></li>
-             <li><a class="footer" href="">Cookie Beleid</a></li>
-             <li><a class="footer" href="">Algemene Verkoopsvoorwaarden</a></li>
-             <li><a class="footer" href="">Privacy Beleid B2B</a></li>
-            </ul>
-            </div>
-            </div>
-            </div>
-</section>
-         <h4 class='footer_copy'>© Copyright Knakworst <?php echo date('Y') ?></h4>
-        </footer>
-    </html>
+        <div class="voorw_foot col-6">
+          <h3>Voorwaarden</h3>
+          <ul class='footer'>
+            <li><a class="footer" href="">Privacy Voorwaarden</a></li>
+            <li><a class="footer" href="">Privacy Beleid</a></li>
+            <li><a class="footer" href="">Cookie Beleid</a></li>
+            <li><a class="footer" href="">Algemene Verkoopsvoorwaarden</a></li>
+            <li><a class="footer" href="">Privacy Beleid B2B</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <h4 class='footer_copy'>© Copyright Knakworst <?php echo date('Y') ?></h4>
+</footer>
+
+</html>
