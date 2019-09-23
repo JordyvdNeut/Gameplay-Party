aff9a36df92a81923ab02fe47dae6d084e8f7da0
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-23 10:27:47 +0200

    created with createDiv function -> c_bioscopen

--- view/overzicht.php
+++ view/overzicht.php
@@ -1,45 +1,15 @@
 <html lang="en">
 
-<?php require_once('header.php'); 
-require_once 'controller/c_bioscopen.php';
-
-$this->c_bioscopen= new biosController();
-$productsTable = $this->c_bioscopen->createTable($result);
-echo $productsTable;
-?>
+<?php require_once('header.php'); ?>
 
 <body>
-  <h2 class="header">Overzicht</h2>
+    <h2 class="header">Bioscopen overzicht</h2>
     <section>
 
-    <div class="center">
-    <div class="row">
-    <div class="col-6">
-    <div class="content">
-                <h1 class="con_title"><?php ?></h1>
-                <p class="con_in"><img class="biosPhoto" src=''><?php ?></p>
-                <button class="accents"><a href='view/.php'>Lees meer</a></button>
-            </div>
-</div>
-
-            <div class=" col-6">
-                <div class="content">
-                <h1 class="con_title"><?php ?></h1>
-                <p class="con_in"><img class="biosPhoto" src=''><?php ?></p>
-                <button class="accents"><a href='view/.php'>Lees meer</a></button>
-            </div>
-</div>
-
-            <div class=" col-6">
-                <div class="content">
-                <h1 class="con_title"><?php ?></h1>
-                <p class="con_in"><img class="biosPhoto" src=''><?php ?></p>
-                <button class="accents"><a href='view/.php'>Lees meer</a></button>
-            </div>
-</div>
-            </div>
-            </div>
+        <?= $biosTable ?>
+        
     </section>
     <?php require_once('view/footer.php'); ?>
 </body>
+
 </html>
