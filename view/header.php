47a883ea4b08e0756a046226cf1a991c8ab18025
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-30 15:34:07 +0200

    format

--- view/header.php
+++ view/header.php
@@ -1,7 +1,7 @@
 <header>
   <nav>
     <div class="not-mobile">
-      <a href="index.php?op=loginForm" ><button class="btn">Login</button></a>
+      <a href="index.php?op=loginForm"><button class="btn">Login</button></a>
       <a href="index.php?op=overons"><button class="btn">Over ons</button></a>
       <a href="index.php?op=overzicht"><button class="btn">Bioscopen</button></a>
       <a href="index.php?op=home"><button class="btn">Home</button></a>
