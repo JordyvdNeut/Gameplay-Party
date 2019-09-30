2f267012da65a5399355cff38a37ec039a4fab2e
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-09-30 12:52:55 +0200

    form removed from the footer

--- view/loginForm.php
+++ view/loginForm.php
@@ -2,33 +2,38 @@
 
 <?php require_once('view/header.php'); ?>
 
-<section class="col-12 row">
+  <section class="row">
 
-  <div class="col-4 content">
-    <h2>Redacteur</h2>
-    <form action="index.php?op=beHome" method="post">
-      <label>Gebruikersnaam</label>
-      <input class="form-control" value="" name="username" type="text" required="required" /><br>
-      <label>Wachtwoord</label>
-      <input class="form-control" name="password" type="password" required="required" />
-      <div class="row">
-        <button type="submit" class="btn" name="submit"><span>Inloggen</span></button> <button type="reset" class="btn"><span>Annuleren</span></button>
-      </div>
-    </form>
-  </div>
+    <div class="col-4 content">
+      <h2>Redacteur</h2>
+      <form action="index.php?op=beHome" method="post">
+        <label>Gebruikersnaam</label>
+        <input class="form-control" value="" name="username" type="text" required="required" />
+        <br />
+        <label>Wachtwoord</label>
+        <input class="form-control" name="password" type="password" required="required" />
+        <br />
+        <div class="row">
+          <button type="submit" class="btn" name="submit">Inloggen</button> <button type="reset" class="btn">Annuleren</button>
+        </div>
+      </form>
+    </div>
 
-  <div class="col-4 content">
-    <h2>Bioscopen</h2>
-    <form action="index.php?op=beHome" method="post">
-      <label>Gebruikersnaam</label>
-      <input class="form-control" value="" name="username" type="text" required="required" /><br>
-      <label>Wachtwoord</label>
-      <input class="form-control" name="password" type="password" required="required" />
-      <div class="row">
-        <button type="submit" class="btn" name="submit"><span>Inloggen</span></button> <button type="reset" class="btn"><span>Annuleren</span></button>
-      </div>
-    </form>
-  </div>
-</section>
+    <div class="col-4 content">
+      <h2>Bioscoop Beheerder</h2>
+      <form action="index.php?op=beHome" method="post">
+        <label>Gebruikersnaam</label>
+        <input class="form-control" value="" name="username" type="text" required="required" />
+        <br />
+        <label>Wachtwoord</label>
+        <input class="form-control" name="password" type="password" required="required" />
+        <br />
+        <div class="row">
+          <button type="submit" class="btn" name="submit">Inloggen</button> <button type="reset" class="btn">Annuleren</button>
+        </div>
+      </form>
+    </div>
+    
+  </section>
 
 <?php require_once('view/footer.php'); ?>
