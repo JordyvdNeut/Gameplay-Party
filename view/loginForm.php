047804444f5399ec8f8b2fdab5bf894f861c5294
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-10-01 15:32:22 +0200

    removed useless extra form

--- view/loginForm.php
+++ view/loginForm.php
@@ -2,10 +2,13 @@
 
 <?php require_once('view/header.php'); ?>
 
-<section class="row">
-
+<section>
+<div>
+  <p>Op deze pagina kunnen beheerders en beheerder van bioscopen inloggen</p>
+</div>
+<div class="row">
   <div class="col-4 content">
-    <h2>Redacteur</h2>
+    <h2>Beheerders login</h2>
     <form action="index.php?op=beHome" method="post">
       <label>Gebruikersnaam</label>
       <input class="form-control" value="" name="username" type="text" required="required" />
@@ -18,21 +21,7 @@
       </div>
     </form>
   </div>
-
-  <div class="col-4 content">
-    <h2>Bioscoop Beheerder</h2>
-    <form action="index.php?op=beHome" method="post">
-      <label>Gebruikersnaam</label>
-      <input class="form-control" value="" name="username" type="text" required="required" />
-      <br />
-      <label>Wachtwoord</label>
-      <input class="form-control" name="password" type="password" required="required" />
-      <br />
-      <div class="row">
-        <button type="submit" class="btn btnLogin" name="submit">Inloggen</button><button type="reset" class="btn btnLogin">Annuleren</button>
-      </div>
-    </form>
-  </div>
+</div>
 
 </section>
 
