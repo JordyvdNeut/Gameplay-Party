275c6893385c7b974a037892eb16dafc5af4452e
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-10-02 12:41:25 +0200

    Alles werkend

--- model/m_beheerdersLogic.php
+++ model/m_beheerdersLogic.php
@@ -67,6 +67,17 @@ class BeheerdersLogic
     }
   }
 
+  public function readAvailabilty()
+  {
+    try {
+      $sql = "SELECT zaal_id,	datum Datum,	beg_tijd 'Begin tijd',	eind_tijd 'Eind tijd' FROM mogelijkheden WHERE beschik = 'flase'";
+      $results = $this->DataHandler->readsData($sql);
+      return $results;
+    } catch (exception $e) {
+      throw $e;
+    }
+  }
+
   public function sendEmail($creating)
   {
     $name           = $creating["name"];
@@ -81,5 +92,4 @@ class BeheerdersLogic
       throw $e;
     }
   }
-
 }
