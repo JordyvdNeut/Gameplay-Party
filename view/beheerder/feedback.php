0851dd1ca63a1dacc4ea5b9ea13aade29393c780
JordyvdNeut <jordyneut@hotmail.com>
2019-10-07 13:08:35 +0200

    een pagina voor alle feedback na het crud

---
+++ view/beheerder/feedback.php
@@ -0,0 +1,7 @@
+<a href="?op=beHome" style="float: left; margin-left: 15px;">
+  <button class="btn">
+    <li class="fa fa-arrow-left"></li> Overzicht
+  </button>
+</a>
+<br /><br />
+<?= $feedback ?>
