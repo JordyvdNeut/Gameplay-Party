54d360e6d507c21d13430f1e8ad8904a31e8ca7b
JordyvdNeut <jordyneut@hotmail.com>
2019-10-07 13:06:06 +0200

    1 table functie voor de redacteur

--- controller/c_htmlBeheerder.php
+++ controller/c_htmlBeheerder.php
@@ -8,58 +8,38 @@ class HTMLBeheerderController
   public function __destruct()
   { }
 
-  public function createHomeConTable($rows)
+  public function createConTable($rows, $header, $link)
   {
     $tableheader = false;
     $html = "";
 
-    $html .= "<h3>Home pagina</h3>";
+    $html .= "<h3>$header</h3>";
     $html .= "<table class='table'>";
 
     foreach ($rows as $row) {
-      $row['Inhoud'] = substr($row['Inhoud'], 0, 250) . "...";
-      while($tableheader !== true) {
-      $html .= "<tr>";
-      $html .= "<th>Titel</th>";
-      $html .= "<th>Inhoud</th>";
-      $html .= "<th>Wijzigen</th>";
-      $html .= "</tr>";
-      $tableheader = true;
-      }
-      $html .= "<tr>";
-      $html .= "<td>{$row['Titel']}</td>";
-      $html .= "<td>{$row['Inhoud']}</td>";
-      $html .= "<td><a href='?op=updateHomeConForm&id=" . $row['id'] . "'><button><span class='glyphicon glyphicon-pencil'></span> Bewerken</button></a></td>";
-      $html .= "</tr>";
-    }
-
-    $html .= "</table>";
-    return $html;
-  }
-
-  public function createOveronsConTable($rows)
-  {
-    $tableheader = false;
-    $html = "";
-
-    $html .= "<h3>Contact pagina</h3>";
-    $html .= "<table class='table'>";
-
-    foreach ($rows as $row) {
-      $row['Over ons'] = substr($row['Over ons'], 0, 250) . "...";
+      // if ($row['Inhoud']) {
+      //   $row['Inhoud'] = substr($row['Inhoud'], 0, 250) . "...";
+      // }
+      // if ($row['Over Ons']) {
+      //   $row['Over ons'] = substr($row['Over ons'], 0, 250) . "...";
+      // }
       $html .= "<tr>";
       if ($tableheader !== true) {
         foreach ($row as $key => $value) {
-          $html .= "<th>{$key}</th>";
+          // if ($value !== $value['id']) {
+            $html .= "<th>{$key}</th>";
+          // }
         }
         $html .= "<th>Wijzigen</th>";
         $html .= "</tr>";
         $tableheader = true;
       }
       foreach ($row as $value) {
-        $html .= "<td>{$value}</td>";
+        // if ($value !== $value['id']) {
+          $html .= "<td>{$value}</td>";
+        // }
       }
-      $html .= "<td><a href=''><button><span class='glyphicon glyphicon-pencil'></span> Bewerken</button></a></td>";
+      $html .= "<td><a href='?op=$link&id=" . $row['id'] . "'><button><span class='glyphicon glyphicon-pencil'></span> Bewerken</button></a></td>";
       $html .= "</tr>";
     }
 
@@ -96,10 +76,12 @@ class HTMLBeheerderController
     return $html;
   }
 
-  public function makeRadio($radio){
-		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-		  $html .= "<input type='radio' name='$row[zaal_id]' placeholder='$row[zaal_nr]'>";  
-		}
-		return $html;
-	  }
+  public function makeRadio($result)
+  {
+    $html = "";
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      $html .= "<input type='radio' name='$row[zaal_id]' placeholder='$row[zaal_nr]'>";
+    }
+    return $html;
+  }
 }
