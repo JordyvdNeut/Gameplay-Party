89347221b6f6e417876dbc71df13cc50a233795e
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-10-09 11:44:33 +0200

    Button value naar Toevoegen veranderd

--- view/beheerder/addBeschik.php
+++ view/beheerder/addBeschik.php
@@ -1,21 +1,21 @@
-<?php require_once('view/beheerder/header.php')?>
+<?php require_once('view/beheerder/header.php') ?>
 <div class="container">
-<div class="row">
-<div class="col-md-12">
-<div class="infocontent bewerken">
-    <form class='form-contact' action='index.php?op=addBeschik' method='POST'>
-        <h1 class='heading'>Voeg uw beschikbaarheid toe</h1>
-            <label>Zaal</label><br>
-            <?= $radioButtons; ?>
-            <label>Datum</label><br>
-            <input type='date' name='datum' id='date' class='form-control' required>
-            <label>Begin tijd</label><br>
-            <input type='time' name='beg_tijd' id='beg_tijd' class='form-control' required>
-            <label>Eind tijd</label><br>
-            <input type='time' name='eind_tijd' id='eind_tijd' class='form-control' required><br>
-        <input class='btn' type='submit'>
-    </form>
-</div>
-</div>
-</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="infocontent bewerken">
+				<form class='form-contact' action='index.php?op=addBeschik' method='POST'>
+					<h1 class='heading'>Voeg uw beschikbaarheid toe</h1>
+					<label>Zaal</label><br>
+					<?= $radioButtons; ?>
+					<label>Datum</label><br>
+					<input type='date' name='datum' id='date' class='form-control' required>
+					<label>Begin tijd</label><br>
+					<input type='time' name='beg_tijd' id='beg_tijd' class='form-control' required>
+					<label>Eind tijd</label><br>
+					<input type='time' name='eind_tijd' id='eind_tijd' class='form-control' required><br>
+					<input class='btn' type='submit' value='Toevoegen'>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
