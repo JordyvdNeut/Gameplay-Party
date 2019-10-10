340b0b094b3e519e140741fd320e28bc438f5553
JurredeBruin <50706153+JurredeBruin@users.noreply.github.com>
2019-10-10 13:34:46 +0200

    print.css aangemaakt

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" 
            integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" type="text/css" href="print.css" media="print" />
   <link rel="stylesheet" href="grid.css">
 </head>
 
