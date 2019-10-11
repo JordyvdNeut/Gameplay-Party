8181885502ffc29e0775145380f9bbbdd444b235
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-10-11 09:54:10 +0200

    view bewerken beschikbaarheid

--- view/beheerder/bioscoop/upBeschik.php
+++ view/beheerder/bioscoop/upBeschik.php
@@ -1,18 +1,31 @@
 <?php require_once('view/beheerder/header.php') ?>
 <div class="col-md-12">
   <div class="content">
-    <form class='form-contact' action='' method='POST'>
-      <h1 class='heading'>Beschikbaarheid aanpassen</h1>
-      <label>Zaal</label><br>
-      <?= $radioButtons; ?>
-      <label>Datum</label><br>
-      <input type='date' name='datum' id='date' class='form-control' required>
-      <label>Begin tijd</label><br>
-      <input type='time' name='beg_tijd' id='beg_tijd' class='form-control' required>
-      <label>Eind tijd</label><br>
-      <input type='time' name='eind_tijd' id='eind_tijd' class='form-control' required><br>
-      <input class='btn' type='submit'>
-    </form>
+
+    <?php
+    $html = "";
+    $id = $_REQUEST['id'];
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      $html .= "<form class='form-contact' action='?op=updateBeschik&id=$id' method='POST'>";
+      $html .= "<h1 class='heading'>Beschikbaarheid aanpassen</h1>";
+      $html .= "<h3>Zaalnr: $row[zaal_nr]</h3>";
+      $html .= "<label>Datum</label>";
+      $html .= "<input class='form-control' type='date' name='datum' value='$row[datum]' />";
+      $html .= "<br />";
+      $html .= "<label>Begin tijd</label>";
+      $html .= "<input class='form-control' type='time' name='beg_tijd' value='$row[beg_tijd]' />";
+      $html .= "<br />";
+      $html .= "<label>Eind tijd</label>";
+      $html .= "<input class='form-control' type='time' name='eind_tijd' value='$row[eind_tijd]' />";
+      $html .= "<br />";
+      $html .= "<label>Nog beschikbaar</label>";
+      $html .= "<input class='form-control' type='checkbox' name='beschik' value='$row[beschik]' />";
+      $html .= "<br />";
+      $html .= "<button type='submit' class='btn'> Bewerken</button>";
+      $html .= "</form>";
+    }
+    echo $html;
+    ?>
   </div>
 </div>
 </div>
