f1ac2cdfd680c07e490b500d5938be5bc6c5058e
JordyvdNeut <jordyneut@hotmail.com>
2019-10-13 12:06:37 +0200

    Replaced to html controller

--- controller/c_bioscopen.php
+++ controller/c_bioscopen.php
@@ -24,114 +24,20 @@ class BiosController
 	public function readBios($id)
 	{
 		$result = $this->bioscopen->readBios($id);
-		$beschikbaar =$this->bioscopen->readBiosBeschik($id);
-		$biosPage = $this->createDetail($result,$beschikbaar);
+		$beschikbaar = $this->bioscopen->readBiosBeschik($id);
+		$biosPage = $this->HTMLController->createBiosDetail($result, $beschikbaar);
 		include 'view/detail.php';
 	}
 
-	// public function update()
-	// {
-	// 	$result = $this->bioscopen->update();
-	// 	include 'view/beheerder/beheerder.php';
-	// }
-
-
-	public function createDiv($result)
-	{
-		$html = "";
-		$html .= "<div class='center'><div class='row'>";
-
-		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			//$row['bios_info'] = wordwrap($row['bios_info'], 500, "<br><br />\n");
-			$html .= "<div class='col-5'>";
-			$html .= "<div class='content'>";
-			$html .= "<h1 class='con_title'>$row[bios_naam]</h1>";
-			$html .= "<p class='con_inh'><img class='biosPhoto' src='$row[bios_foto]'><br />";
-			$html .= "$row[bios_info]...</p><br />";
-			
-			$html .= "<a href='index.php?op=detail&id=$row[bios_id]'><button class='btn'>Lees meer</button></a>";
-			$html .= "</div>";
-			$html .= "</div>";
-		}
-
-		$html .= "</div></div>";
-		return $html;
-	}
-
-	public function createDetail($result,$beschikbaar)
-	{
-		$html = "";
-		$html .= "<section class='row'>";
-
-		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			//$row['bios_info'] = wordwrap($row['bios_info'], 500, "<br><br />\n");
-			$html .= "<div class='col-9'>";
-			$html .= "<div class='content'>";
-			$html .= "<h2>$row[bios_naam]</h2>";
-			$html .= "<p class='con_in'><img class='biosPhoto' src='$row[bios_ins]'></p>";
-			$html .= "<p>$row[bios_info]</p>";
-			if ($row['bios_diensten'] !== null) {
-				$html .= "<h2>Extra mogelijkheden: </h2>";
-				$html .= "$row[bios_diensten]";
-			}
-			$html .= "</div>";
-			$html .= "</div>";
-
-			
-			$html .= "<div class='col-2'>";
-			$html .= "<div class='content'>";
-			$html .= "<h3><strong>Contact gegevens</strong></h3>";
-			$html .= "<h4><strong>Adres: </strong></h4>";
-			$html .= "<p>$row[bios_adres]<br />";
-			$html .= "$row[bios_plaats]</p>";
-			$html .= "<p>Telefoon nummer: $row[bios_tel]</p>";
-			$html .= "</div>";
-			$html .= "</div>";
-		}
-		$html .= "<div class='col-12'>";
-		$html .= "<div class='beschikcontent'>";
-		$html .="<h4><strong>Beschikbare zalen</strong></h4>";
-		$html .= "</div>";
-		$html .= "</div>";
-		while ($row = $beschikbaar->fetch(PDO::FETCH_ASSOC)) {
-			$gooddate= date("d-m-Y", strtotime($row['datum']));
-			$begintime= date("H:i ", strtotime($row['beg_tijd']));
-			$endtime= date("H:i ", strtotime($row['eind_tijd']));
-			$html .= "<div class='col-lg-4' style='margin-bottom:15px;'>";
-			$html .= "<div class='infocontent'>";
-
-			$html .= "<h4><strong>Zaal $row[zaal_nr]</strong></h4>";
-			$html .= "<p>Datum: $gooddate </p>";
-			$html .= "<p>Tijd: $begintime - $endtime</p>";
-			$html .= "<p>Aantal stoelen: $row[plaatsen]</p>";
-			if("$row[invalide]"==1){
-				$html .= "<p>Invalide toegankelijk: Ja </p>";
-			}else{
-				$html .= "<p>Invalide toegankelijk: Nee</p>";
-			}
-			$html .="<button class='btn'>Reserveren binnenkort beschikbaar</button>";
-			$html .= "</div>";
-			$html .= "</div>";
-		}
-		// $html .= "<div class='col-9'>";
-		// $html .= "<div class='content'>";
-		// 	$html .= "<h2>Extra mogelijkheden: </h2>";
-		// $html .= "</div>";
-		// $html .= "</div>";
-		$html .= "</section>";
-		return $html;
-	}
-
-	public function updateBios() 
+	public function updateBios()
 	{
 		$result = $this->bioscopen->updateBios();
 		include 'view/beheerder/updatedBios.php';
 	}
 
-	public function deleteBios() 
+	public function deleteBios()
 	{
 		$result = $this->bioscopen->delete();
 		include 'view/beheerder/deleteBios.php';
 	}
-
 }
--- controller/c_html.php
+++ controller/c_html.php
@@ -7,40 +7,64 @@ class HTMLController
 
   public function __destruct()
   { }
-  public function createBiosDetail($result)
-  {
-    $html = "";
-    $html .= "<section class='row'>";
+  
+	public function createBiosDetail($result, $beschikbaar)
+	{
+		$html = "";
+		$html .= "<section class='row'>";
 
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      $html .= "<div class='col-8'>";
-      $html .= "<div class='content'>";
-      $html .= "<h2>$row[bios_naam]</h2>";
-      $html .= "<p class='con_in'><img class='biosInside' src='$row[bios_ins]'></p>";
-      $html .= "<p>$row[bios_info]</p>";
-      if ($row['bios_diensten'] !== null) {
-        $html .= "<h2>Extra mogelijkheden: </h2>";
-        $html .= "$row[bios_diensen]";
-      }
-      $html .= "</div>";
-      $html .= "</div>";
+		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			// $row['bios_info'] = wordwrap($row['bios_info'], 500, "<br><br />\n");
+			$html .= "<div class='col-9'>";
+			$html .= "<div class='content'>";
+			$html .= "<h2>$row[bios_naam]</h2>";
+			$html .= "<p class='con_in'><img class='biosPhoto' src='$row[bios_ins]'></p>";
+			$html .= "<p>$row[bios_info]</p>";
+			$html .= "<h4><strong>Aanbetaling</strong></h4>";
+			$html .= "<p>Na dat er een reservering is gemaakt word er verwacht dat er bij $row[bios_naam] een aanbetaling word gedaan binnen 48 uur na de reserviring. Dit is nodig om de bestelling te voltooien.</p>";
+			$html .= "</div>";
+			$html .= "</div>";
 
+			$html .= "<div class='col-2'>";
+			$html .= "<div class='content'>";
+			$html .= "<h3><strong>Contact gegevens</strong></h3>";
+			$html .= "<h4><strong>Adres: </strong></h4>";
+			$html .= "<p>$row[bios_adres]<br />";
+			$html .= "$row[bios_plaats]</p>";
+			$html .= "<h4><strong>Telefoon nummer: </strong></h4>";
+			$html .= "<p>$row[bios_tel]</p>";
+			$html .= "</div>";
+			$html .= "</div>";
+		}
+		$html .= "<div class='col-12'>";
+		$html .= "<div class='beschikcontent'>";
+		$html .= "<h4><strong>Beschikbare zalen</strong></h4>";
+		$html .= "</div>";
+		$html .= "</div>";
+		while ($row = $beschikbaar->fetch(PDO::FETCH_ASSOC)) {
+			$gooddate = date("d-m-Y", strtotime($row['datum']));
+			$begintime = date("H:i ", strtotime($row['beg_tijd']));
+			$endtime = date("H:i ", strtotime($row['eind_tijd']));
+			$html .= "<div class='col-lg-4' style='margin-bottom:15px;'>";
+			$html .= "<div class='infocontent'>";
 
-      $html .= "<div class='col-3'>";
-      $html .= "<div class='content'>";
-      $html .= "<h3><strong>Contact gegevens</strong></h3>";
-      $html .= "<h4><strong>Adres: </strong></h4>";
-      $html .= "<p>$row[bios_adres]<br />";
-      $html .= "$row[bios_plaats]</p>";
-      $html .= "<p>Telefoon nummer: $row[bios_tel]</p>";
-      $html .= "</div>";
-      $html .= "</div>";
-    }
-
-    $html .= "</section>";
-    return $html;
+			$html .= "<h4><strong>Zaal $row[zaal_nr]</strong></h4>";
+			$html .= "<p>Datum: $gooddate </p>";
+			$html .= "<p>Tijd: $begintime - $endtime</p>";
+			$html .= "<p>Aantal stoelen: $row[plaatsen]</p>";
+			if ("$row[invalide]" == 1) {
+				$html .= "<p>Invalide toegankelijk: Ja </p>";
+			} else {
+				$html .= "<p>Invalide toegankelijk: Nee</p>";
+			}
+			$html .= "<button class='btn'>Reserveren binnenkort beschikbaar</button>";
+			$html .= "</div>";
+			$html .= "</div>";
+		}
+		$html .= "</section>";
+		return $html;
   }
-
+  
   public function createBiosDiv($result)
   {
     $html = "";
