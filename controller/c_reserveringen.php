c5eec6e8cab21fd66fbf72771759589868f8181c
JordyvdNeut <jordyneut@hotmail.com>
2019-10-14 14:03:40 +0200

    getfactuur

--- controller/c_reserveringen.php
+++ controller/c_reserveringen.php
@@ -1,11 +1,13 @@
 <?php
-require_once('m_reserveringen.php');
+require_once 'model/m_reserveringen.php';
+require_once 'controller/c_bioscopen.php';
 
 class resController
 {
 	public function __construct()
 	{
-		$this->reserveringen = new reserveringen();
+		$this->reserveringenModel = new reserveringenModel();
+		$this->biosModel = new Bioscopen();
 	}
 
 	public function __destruct()
@@ -20,4 +22,20 @@ class resController
 			$errors = $e->getErrors();
 		}
 	}
+
+	public function	getFactuur()
+	{
+		$_REQUEST['res_date'] = date("d/m/Y");
+		$factuur = $_REQUEST;
+		$zaalGegevens = $this->reserveringenModel->getZaalDetail($_REQUEST['id']);
+		$bestelDetails = $this->reserveringenModel->getBestDetail($_REQUEST['id']);
+		$tarieven = $this->reserveringenModel->getTarieven();
+		include 'view/factuur.php';
+	}
+  
+  public function getResForm($id)
+  {
+    $biosDetail = $this->biosModel->readBios($id);
+		include "view/resForm.php";
+  }
 }
