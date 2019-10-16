3311fbd8f5d0eaa5351f3a5debdffff68b15b924
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-10-16 13:53:26 +0200

    commends XD

--- view/resForm.php
+++ view/resForm.php
@@ -53,97 +53,98 @@ require_once('view/header.php');
         $html .= "</tr>";
         $html .= "</table>";
       }
-      $html .= "<br />";
+      $html .= "<hr />";
       echo $html;
       ?>
       <div class="row">
-      <div class="col-5 rescontent">
-      <form action="index.php?op=reserveren&id=<?=$_REQUEST['id']?>&bios=<?=$_REQUEST['bios']?>" method="post">
-        <label>Uw voornaam</label>
-        <input class="form-control" name="voornaam" type="text" required="required" />
-        <br />
-        <label>Tussenvoegsel</label>
-        <input class="form-control" name="tussenvoegsel" type="text" required="required" />
-        <br />
-        <label>Achternaam</label>
-        <input class="form-control" name="achternaam" type="text" required="required" />
-        <br />
-        <label>Woonplaats</label>
-        <input class="form-control" name="woonplaats" type="text" required="required" />
-        <br />
-        <label>Adres</label>
-        <input class="form-control" name="adres" type="text" required="required" />
-        <br />
-        <label>Postcode</label>
-        <input class="form-control" name="postcode" type="text" required="required" />
-        <br />
-        <label>Huisnummer</label>
-        <input class="form-control" name="adres" type="number" required="required" />
-        <br />
-        <label>Telefoon nummer</label>
-        <input class="form-control" name="adres" type="tel" required="required" />
-        <br />
-        <hr />
-        <br />
-</div>
+        <!-- Klant gegevens toevoegen -->
         <div class="col-5 rescontent">
-        <label>18 t/m 64 zonder bijzonderheden</label>
-        <select name="normaal" class="form-control">
-          <option value="">--Geen 18 tot 65 jaar--</option>
-          <?php
-          foreach (range(1, 50) as $number) {
-            echo "<option value='" . $number . "'>" . $number . "</option>";
-          }
-          ?>
-        </select>
-        <br />
-        <label>Aantal kinderen t/m 11 jaar</label>
-        <select name="tm11" class="form-control">
-          <option value="">--Geen kinderen--</option>
-          <?php
-          foreach (range(1, 50) as $number) {
-            echo "<option value='" . $number . "'>" . $number . "</option>";
-          }
-          ?>
-        </select>
-        <br />
-        <label>Jeugd 12 t/m 17 jaar</label>
-        <select name="12tm17" class="form-control">
-          <option value="">--Geen jeugd--</option>
-          <?php
-          foreach (range(1, 50) as $number) {
-            echo "<option value='" . $number . "'>" . $number . "</option>";
-          }
-          ?>
-        </select>
-        <br />
-        <label>65+</label>
-        <select name="65+" class="form-control">
-          <option value="">--Geen 65 plussers--</option>
-          <?php
-          foreach (range(1, 50) as $number) {
-            echo "<option value='" . $number . "'>" . $number . "</option>";
-          }
-          ?>
-        </select>
-        <br />
-        <label>Studenten, CJP & BankGiro Loterij VIP-KAART</label>
-        <select name="overig" class="form-control">
-          <option value="">--Geen Studenten, CJP of BankGiro Loterij VIP-KAART--</option>
-          <?php
-          foreach (range(1, 50) as $number) {
-            echo "<option value='" . $number . "'>" . $number . "</option>";
-          }
-          ?>
-        </select>
-        <br />
-        <div class="row flexnone">
-          <div class='resbtn'>
-          <button type="submit" class="btn">Reserveren</button><button type="reset" class="btn">Annuleren</button>
+          <form action="index.php?op=reserveren&id=<?= $_REQUEST['id'] ?>&bios=<?= $_REQUEST['bios'] ?>" method="post">
+            <label>Uw voornaam</label>
+            <input class="form-control" name="voornaam" type="text" required="required" />
+            <br />
+            <label>Tussenvoegsel</label>
+            <input class="form-control" name="tussenvoegsel" type="text" required="required" />
+            <br />
+            <label>Achternaam</label>
+            <input class="form-control" name="achternaam" type="text" required="required" />
+            <br />
+            <label>Woonplaats</label>
+            <input class="form-control" name="woonplaats" type="text" required="required" />
+            <br />
+            <label>Adres</label>
+            <input class="form-control" name="adres" type="text" required="required" />
+            <br />
+            <label>Postcode</label>
+            <input class="form-control" name="postcode" type="text" required="required" />
+            <br />
+            <label>Huisnummer</label>
+            <input class="form-control" name="adres" type="number" required="required" />
+            <br />
+            <label>Telefoon nummer</label>
+            <input class="form-control" name="adres" type="tel" required="required" />
+            <br />
+            <br />
         </div>
+        <!-- Personen toevoegen -->
+        <div class="col-5 rescontent">
+          <label>18 t/m 64 zonder bijzonderheden</label>
+          <select name="normaal" class="form-control">
+            <option value="">--Geen 18 tot 65 jaar--</option>
+            <?php
+            foreach (range(1, 50) as $number) {
+              echo "<option value='" . $number . "'>" . $number . "</option>";
+            }
+            ?>
+          </select>
+          <br />
+          <label>Aantal kinderen t/m 11 jaar</label>
+          <select name="tm11" class="form-control">
+            <option value="">--Geen kinderen--</option>
+            <?php
+            foreach (range(1, 50) as $number) {
+              echo "<option value='" . $number . "'>" . $number . "</option>";
+            }
+            ?>
+          </select>
+          <br />
+          <label>Jeugd 12 t/m 17 jaar</label>
+          <select name="12tm17" class="form-control">
+            <option value="">--Geen jeugd--</option>
+            <?php
+            foreach (range(1, 50) as $number) {
+              echo "<option value='" . $number . "'>" . $number . "</option>";
+            }
+            ?>
+          </select>
+          <br />
+          <label>65+</label>
+          <select name="65+" class="form-control">
+            <option value="">--Geen 65 plussers--</option>
+            <?php
+            foreach (range(1, 50) as $number) {
+              echo "<option value='" . $number . "'>" . $number . "</option>";
+            }
+            ?>
+          </select>
+          <br />
+          <label>Studenten, CJP & BankGiro Loterij VIP-KAART</label>
+          <select name="overig" class="form-control">
+            <option value="">--Geen Studenten, CJP of BankGiro Loterij VIP-KAART--</option>
+            <?php
+            foreach (range(1, 50) as $number) {
+              echo "<option value='" . $number . "'>" . $number . "</option>";
+            }
+            ?>
+          </select>
+          <br />
+          <div class="row flexnone">
+            <div class='resbtn'>
+              <button type="submit" class="btn">Reserveren</button><button type="reset" class="btn">Annuleren</button>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
       </form>
     </div>
 
